3ec654948bc4a87dbe8509f8ffaa80c93c75d109
Sadini <jasingha2002sadninipunika@gamil.com>
2025-10-04 15:48:50 +0530

    .env

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+//vLJia54jUJgCWacq
+//mongodb+srv://<db_username>:<db_password>@cluster0.zdcdipd.mongodb.net/
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT
+, () => {
     console.log('Server is running on port 3000');
 }   );
